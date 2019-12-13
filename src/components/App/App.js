62b02a8776f29108ff427fc07206813fea92b85b
Alyssa Lundgren <lundgrea@gmail.com>
2019-12-13 16:23:15 -0700

    Add Login, Map, and Menu Router links within in and file structure

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import logo from '../../images/logo.png'
+import logo from '../../images/logo.png';
+import { Route } from 'react-router-dom';
+import Menu from '../Menu/Menu';
+import Login from '../Login/Login';
+import Map from '../Map/Map';
 import './App.css';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img className="logo" src={logo} alt="logo"/>
+    <div className='App'>
+      <header className='App-header'>
+        <img className='logo' src={logo} alt="logo"/>
         <h1>Narkal</h1>
-        <button>Narkal Map</button>
-        <button>Log In/Sign Up</button>
-        <button>Find Out More</button>
       </header>
+      <Route exact path='/' component={Menu} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/map' component={Map} />
+       
     </div>
   );
 }
---
+++ src/components/Map/Map.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Map.css';
+
+export const Map = () => {
+  return (
+    <h1>Map</h1>
+  );
+}
+
+export default Map;
